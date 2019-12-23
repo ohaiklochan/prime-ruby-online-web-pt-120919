d8641ce162a2a4774667b3f792c0f3e891569ca6
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 01:01:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |x|
+      number % x == 0
+    end
+  else
+    false
+  end
+end
